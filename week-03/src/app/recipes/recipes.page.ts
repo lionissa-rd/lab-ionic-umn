bbc5f76e61bf307c51ce0a20efb4d0c308f10363
lionissa-rd <lionissa.darmawan@gmail.com>
2019-08-28 16:37:04 +0700

    commit week2 & week3

---
+++ week-03/src/app/recipes/recipes.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.model';
+import { RecipesService } from './recipes.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+
+  recipes: Recipe[];
+
+  constructor(private recipesService: RecipesService) { }
+
+  ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  getRecipe(recipeId : string)
+  {
+    this.recipesService.getRecipe(recipeId);
+  }
+
+  deleteRecipe(recipeId : string)
+  {
+    this.recipesService.deleteRecipe(recipeId);
+  }
+
+}
---
+++ week-03/src/app/recipes/recipes.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipes.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private recipes: Recipe[] = 
+  [
+    {
+      id: 'r1',
+      title: 'Gado-gado',
+      imageUrl: 'http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/05/gado-gado-salad.jpg?itok=MTTSriC8',
+      ingredients: ['Lontong', 'Sawi', 'Bumbu Kecap', 'Tauge']
+    },
+    {
+      id: 'r2',
+      title: 'Ketupat',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Ketupat.jpg',
+      ingredients: ['Nasi', 'Air']
+    },
+    {
+      id: 'r3',
+      title: 'Pizza Margherita',
+      imageUrl: 'https://imgp2.schaer.com/sites/default/files/2017-09/HeaderProducts_Pizza%20Margherita.jpg',
+      ingredients: ['Tepung', 'Daun Basil', 'Saus Margherita']
+    }
+
+  ];
+
+  constructor() { }
+
+  copyRecipe()
+  {
+    
+    // return {...this.recipes.find(recipe => {return recipe.id === recipeId})};
+  }
+
+  getAllRecipes()
+  {
+    return [...this.recipes];
+    //... -> All
+
+    //return this.copyRecipe();
+  }
+
+  getRecipe(recipeId: string)
+  {
+    //typescript .find for shortcut
+
+    return{
+      ...this.recipes.find(recipe =>{
+          return recipe.id === recipeId;
+      })
+    };
+    
+    //this.recipes = this.recipes.filter(recipe=>{return recipe.id !== recipeId;});
+    //"buat sebuah array dengan anggota yang sama dengan array sebelumnya TANPA (!==) anggota yang memenuhi syarat berikut"
+  }
+
+  deleteRecipe(recipeId: string)
+  {
+    //typescript .filter for shortcut
+
+    this.recipes = this.recipes.filter(recipe =>{
+      return recipe.id !== recipeId;
+    });
+  }
+
+
+}
