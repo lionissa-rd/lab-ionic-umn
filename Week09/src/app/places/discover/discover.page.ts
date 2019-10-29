1abb253ccdb1f917574bdd9b0c272dcaa02cc373
lionissa-rd <lionissa.darmawan@gmail.com>
2019-10-29 12:43:25 +0700

    Week 09

---
+++ Week09/src/app/places/discover/discover.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, onDestroy } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+import { SegmentChangeEventDetail } from '@ionic/core';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-discover',
+  templateUrl: './discover.page.html',
+  styleUrls: ['./discover.page.scss'],
+})
+export class DiscoverPage implements OnInit, onDestroy {
+  loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
+  private placesSub: Subscription;
+
+  constructor(private placesService: PlacesService) { }
+
+  ngOnInit() {
+    this.placesSub = this.placesService.places.subscribe(places => {
+      this.loadedPlaces = places;
+      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+    });
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
+  }
+
+  ngOnDestroy()
+  {
+    if(this.placesSub)
+    {
+      this.placesSub.unsubscribe();
+    }
+  }
+
+}
---
+++ Week09/src/app/places/offers/offers.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, onDestroy } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-offers',
+  templateUrl: './offers.page.html',
+  styleUrls: ['./offers.page.scss'],
+})
+export class OffersPage implements OnInit, onDestroy {
+  offers: Place[];
+  private placesSub: Subscription;
+
+  constructor(private placesService: PlacesService, private router: Router) { }
+
+  ngOnInit() {
+    this.placesSub = this.placesService.subscribe(places => {
+      this.offers = places;
+    });
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
+  }
+
+  ngOnDestroy()
+  {
+    this.placesSub.unsubscribe();
+  }
+
+}
